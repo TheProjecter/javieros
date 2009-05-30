01a34c030108719f2b3bd74d4aed11a5414f8f9d
c.j.w.chen@gmail.com <c.j.w.chen@gmail.com@f5997d14-48d9-11de-a3b0-c1e83b3b5a46>
2009-05-30 15:32:56 +0000

    GDT ok, IDT ok, Paging enabled...but, qemu error....

---
+++ trunk/include/asm/desc.h
@@ -0,0 +1,8 @@
+#ifndef __DESC_H
+#define __DESC_H
+
+#define NR_CPUS		1
+#define IDTENTRIES	256
+#define GDTENTRIES	(8 + 2 * NR_CPUS)
+
+#endif /* __DESC_H */
---
+++ trunk/kernel/memory.c
@@ -0,0 +1,9 @@
+#include <multiboot.h>
+
+
+/* stored multiboot information */
+char mbi_buffer[MBI_ROUNDEDUP_SIZE];
+multiboot_info_t *mbi = (multiboot_info_t *)mbi_buffer;
+memory_map_t mmap_table[MMAP_TABSIZE] = {{0,0,0,0,0,0},};
+int mmap_table_length = -1;
+
