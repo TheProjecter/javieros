3176b30e0ea38a52084e1e407bd531124d025c81
c.j.w.chen@gmail.com <c.j.w.chen@gmail.com@f5997d14-48d9-11de-a3b0-c1e83b3b5a46>
2009-05-31 07:44:24 +0000

    Add multiboot.o(.text)->.lds

--- trunk/init/init.c
+++ trunk/init/init.c
@@ -1,7 +1,10 @@
 #include <multiboot.h>
 
 unsigned long multiboot_magic = -1;
-/*
+
+int init_memory(void);
+//int init_
+
 void start_kernel()
 {
 	if (multiboot_magic != MULTIBOOT_BOOTLOADER_MAGIC)
@@ -9,4 +12,3 @@ void start_kernel()
 		return;
 	}
 }
-*/
