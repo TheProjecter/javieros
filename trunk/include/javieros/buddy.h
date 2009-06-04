3cc034761d123caa7e6120e1ff84bbaa89d5bdd8
c.j.w.chen@gmail.com <c.j.w.chen@gmail.com@f5997d14-48d9-11de-a3b0-c1e83b3b5a46>
2009-06-04 09:23:04 +0000

    0

--- trunk/include/javieros/buddy.h
+++ trunk/include/javieros/buddy.h
@@ -1,6 +1,14 @@
 #ifndef __BUDDY_H
 #define __BUDDY_H
 
+#ifndef __ASM__
 
+#define MIN_PHY_MEMSIZE (32 << 20)
+#define MAX_PHY_MEMSIZE (896 << 20)
+
+
+#define MAX_PAGEFRAME_NR (MAX_PHY_MEMSIZE >> PAGE_SHIFT)
+
+#endif
 
 #endif
